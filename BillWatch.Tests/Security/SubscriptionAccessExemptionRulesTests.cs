67a1f5c453dcdb648301f8e9ae18aaf746912005
RealizmModz <74314445+RealizmModz@users.noreply.github.com>
2026-09-01 22:36:38 -0600

    Test subscription exemption matrix

---
+++ BillWatch.Tests/Security/SubscriptionAccessExemptionRulesTests.cs
@@ -0,0 +1,42 @@
+using BillWatch.API.Authorization;
+using BillWatch.API.Controllers;
+
+namespace BillWatch.Tests.Security;
+
+public sealed class SubscriptionAccessExemptionRulesTests
+{
+    [Theory]
+    [InlineData(typeof(SubscriptionController), "GetStatus")]
+    [InlineData(typeof(SubscriptionController), "Redeem")]
+    [InlineData(typeof(AdminSecurityController), "AnyAdminAction")]
+    [InlineData(typeof(AdminSubscriptionController), "AnyAdminAction")]
+    [InlineData(typeof(AdminUsersController), "AnyAdminAction")]
+    [InlineData(typeof(AccountController), nameof(AccountController.ExportAccountData))]
+    [InlineData(typeof(AccountController), nameof(AccountController.DeleteAccount))]
+    [InlineData(typeof(BankConnectionsController), nameof(BankConnectionsController.Disconnect))]
+    public void IsExempt_ReturnsTrueOnlyForRequiredEscapeHatches(
+        Type controllerType,
+        string actionName)
+    {
+        Assert.True(
+            SubscriptionAccessExemptionRules.IsExempt(
+                controllerType,
+                actionName));
+    }
+
+    [Theory]
+    [InlineData(typeof(AccountController), "FutureAccountAction")]
+    [InlineData(typeof(BankConnectionsController), nameof(BankConnectionsController.GetConnections))]
+    [InlineData(typeof(BankAccountsController), "AnyAction")]
+    [InlineData(typeof(BankTransactionsController), "AnyAction")]
+    [InlineData(typeof(BillStreamsController), "AnyAction")]
+    public void IsExempt_ReturnsFalseForProtectedFinancialActions(
+        Type controllerType,
+        string actionName)
+    {
+        Assert.False(
+            SubscriptionAccessExemptionRules.IsExempt(
+                controllerType,
+                actionName));
+    }
+}
